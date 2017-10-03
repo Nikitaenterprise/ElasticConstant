474f8d28bc1743ddb7cebbc085cb58ce46f09fb7
Nikitaenterprise <prof.frimen@mail.ru>
2017-10-03 14:05:43 +0300

    Commit #3

--- ElasticConstantC++/ElasticConstantC++.cpp
+++ ElasticConstantC++/ElasticConstantC++.cpp
@@ -4,35 +4,38 @@
 #include "stdafx.h"
 
 void CreatingCrystall(double *X, double *Y, double *Z, size_t size, double latticeParameter);
-void CreatingVacancy(double *X, double *Y, double *Z, size_t size);
+void CreatingVacancy(double *X, double *Y, double *Z, size_t size, std::vector<double> &vacancy);
 double F(double r);
 double E(double *X, double *Y, double *Z, size_t size, int e);
 double Energy(double *X, double *Y, double *Z, size_t size);
-void SettingLatticeParameter(double *X, double *Y, double *Z, size_t size);
+double SettingLatticeParameter(double *X, double *Y, double *Z, size_t size);
 void PrintingMassive(double *X, double *Y, double *Z, size_t size);
+int *NearestToVacancy(double *X, double *Y, double *Z, size_t size, double Xcentral, double Ycentral, double Zcentral, double latticeParameter, size_t &sizeOfIndex);
+void MakingSphere(double *X, double *Y, double *Z, size_t &size, std::vector<double> &vacancy, double latticeParameter);
 
 int main()
 {
-	size_t parameter = 10;
-	size_t size = static_cast <size_t> (pow(parameter, 3));
+	size_t parameter = 14;
+	size_t size = static_cast<size_t> (pow(parameter, 3));
 	double	*X = new double[size],
 			*Y = new double[size],
 			*Z = new double[size];
 
-	//std::vector <double> arr1;
-	//std::vector <double*> arr2;
-
-	//std::vector <std::vector<double>> arr3;
-	//arr3.push_back(std::vector <double>());
-	//arr3[arr3.size() - 1].push_back(100);
-
-
-	SettingLatticeParameter(X, Y, Z, size);
+	std::vector<double> vacancy;
+	vacancy.push_back(0);
+	vacancy.push_back(0);
+	vacancy.push_back(0);
+	
+	double latticeParameter = SettingLatticeParameter(X, Y, Z, size);
 
 	double energyWithoutDefect = Energy(X, Y, Z, size);
-	CreatingVacancy(X, Y, Z, size);
+	CreatingVacancy(X, Y, Z, size, vacancy);
 	double energyWithDefect = Energy(X, Y, Z, size);
 
+	std::cout << "size = " << size << std::endl;
+	MakingSphere(X, Y, Z, size, vacancy, latticeParameter);
+	std::cout << "size = " << size << std::endl;
+	
 	std::cout << energyWithoutDefect << "\t" << energyWithDefect << std::endl;
 	system("pause");
     return 0;
@@ -56,41 +59,135 @@ void CreatingCrystall(double *X, double *Y, double *Z, size_t size, double latti
 	}
 }
 
-void SettingLatticeParameter(double *X, double *Y, double *Z, size_t size)
+double SettingLatticeParameter(double *X, double *Y, double *Z, size_t size)
 {
 	double a0 = 2.845, h = a0 / 20;
 	double latticeParameter = a0;
-	int Nc = static_cast <int> (pow(size, 1.0 / 3.0) / 2 + pow(size, 2.0 / 3.0) / 2 + size / 2);
+	int central = static_cast<int> (pow(size, 1.0 / 3.0) / 2 + pow(size, 2.0 / 3.0) / 2 + size / 2);
 	CreatingCrystall(X, Y, Z, size, latticeParameter);
-	double U0 = E(X, Y, Z, size, Nc);
+	double U0 = E(X, Y, Z, size, central);
 	for (int i = 0; i < 1000; i++)
 	{
 		latticeParameter += h;
 		CreatingCrystall(X, Y, Z, size, latticeParameter);
-		if (E(X, Y, Z, size, Nc) == U0) break;
-		if (E(X, Y, Z, size, Nc) >= U0)
+		if (E(X, Y, Z, size, central) == U0) break;
+		if (E(X, Y, Z, size, central) >= U0)
 		{
 			latticeParameter -= 2 * h;
 			CreatingCrystall(X, Y, Z, size, latticeParameter);
-			U0 = E(X, Y, Z, size, Nc);
+			U0 = E(X, Y, Z, size, central);
 			h /= 8;
 		}
-		if (E(X, Y, Z, size, Nc) < U0) U0 = E(X, Y, Z, size, Nc);
+		if (E(X, Y, Z, size, central) < U0) U0 = E(X, Y, Z, size, central);
 	}
-
+	return latticeParameter;
 }
 
-void CreatingVacancy(double *X, double *Y, double *Z, size_t size)
+void CreatingVacancy(double *X, double *Y, double *Z, size_t size, std::vector<double> &vacancy)
 {
-	int Nc = static_cast <int> (pow(size, 1.0 / 3.0) / 2 + pow(size, 2.0 / 3.0) / 2 + size / 2);
-	X[Nc] = 1000000;
-	Y[Nc] = 1000000;
-	Z[Nc] = 1000000;
+	int central = static_cast <int> (pow(size, 1.0 / 3.0) / 2 + pow(size, 2.0 / 3.0) / 2 + size / 2);
+	vacancy[0] = X[central];
+	vacancy[1] = Y[central];
+	vacancy[2] = Z[central];
+	X[central] = 1000000;
+	Y[central] = 1000000;
+	Z[central] = 1000000;
 }
 
 void PrintingMassive(double *X, double *Y, double *Z, size_t size)
 {
-	for (unsigned int i = 0; i < size; i++) std::cout << "i = " << i << "	X = " << X[i] << std::endl;
+	for (unsigned int i = 0; i < size; i++)	std::cout << "i = " << i << "\tX = " << X[i] << "\tY = " << Y[i] << "\tZ = " << Z[i] << std::endl;
+}
+
+void MakingSphere(double *X, double *Y, double *Z, size_t &size, std::vector<double> &vacancy, double latticeParameter)
+{
+	double	*tempX = new double[size],
+			*tempY = new double[size],
+			*tempZ = new double[size];
+	double Xcentral = vacancy[0], Ycentral = vacancy[1], Zcentral = vacancy[2];
+	std::cout << Xcentral << "\t" << Ycentral << "\t" << Zcentral << std::endl;
+	int counter = 0;
+	for (unsigned int i = 0; i < size; i++)
+	{
+		double R = sqrt(pow((Xcentral - X[i]), 2) + pow((Ycentral - Y[i]), 2) + pow((Zcentral - Z[i]), 2));
+		if (R <= latticeParameter * pow(size, 1.0 / 3.0) / 2 + 0.001)
+		{
+			tempX[counter] = X[i];
+			tempY[counter] = Y[i];
+			tempZ[counter] = Z[i];
+			counter += 1;
+		}
+	}
+
+	delete[] X, Y, Z;
+	X = new double[counter];
+	Y = new double[counter];
+	Z = new double[counter];
+
+	for (int i = 0; i < counter; i++)
+	{
+		X[i] = tempX[i];
+		Y[i] = tempY[i];
+		Z[i] = tempZ[i];
+	}
+	
+	delete[] tempX, tempY, tempZ;
+	tempX = new double[counter];
+	tempY = new double[counter];
+	tempZ = new double[counter];
+
+	int newCounter = 0;
+
+	for (int i = 0; i < counter; i++)
+	{
+		double R = sqrt(pow((Xcentral - X[i]), 2) + pow((Ycentral - Y[i]), 2) + pow((Zcentral - Z[i]), 2));
+		if (R <= latticeParameter * 5)
+		{
+			tempX[newCounter] = X[i];
+			tempY[newCounter] = Y[i];
+			tempZ[newCounter] = Z[i];
+			newCounter += 1;
+		}
+	}
+	for (int i = 0; i < counter; i++)
+	{
+		double R = sqrt(pow((Xcentral - X[i]), 2) + pow((Ycentral - Y[i]), 2) + pow((Zcentral - Z[i]), 2));
+		if (R <= latticeParameter * pow(size, 1.0 / 3.0) / 2 + 0.001 && R > latticeParameter * 5)
+		{
+			tempX[newCounter] = X[i];
+			tempY[newCounter] = Y[i];
+			tempZ[newCounter] = Z[i];
+			newCounter += 1;
+		}
+	}
+	std::cout << "counters = " << counter << "\t" << newCounter << std::endl;
+	delete[] tempX, tempY, tempZ;
+	size = newCounter;
+	size_t sizeOfIndex;
+	int *index = NearestToVacancy(X, Y, Z, size, Xcentral, Ycentral, Zcentral, latticeParameter, sizeOfIndex);
+}
+
+int *NearestToVacancy(double *X, double *Y, double *Z, size_t size, double Xcentral, double Ycentral, double Zcentral, double latticeParameter, size_t &sizeOfIndex)
+{
+	int counter = 0;
+	for (unsigned int i = 0; i < size; i++)
+	{
+		double R = sqrt(pow((Xcentral - X[i]), 2) + pow((Ycentral - Y[i]), 2) + pow((Zcentral - Z[i]), 2));
+		if (R >= latticeParameter * 2 && R <= latticeParameter * 3)	counter += 1;
+	}
+	int *index = new int[counter];
+	counter = 0;
+	for (unsigned int i = 0; i < size; i++)
+	{
+		double R = sqrt(pow((Xcentral - X[i]), 2) + pow((Ycentral - Y[i]), 2) + pow((Zcentral - Z[i]), 2));
+		if (R >= latticeParameter * 2 && R <= latticeParameter * 3)
+		{
+			index[counter] = i;
+			counter += 1;
+		}
+	}
+	sizeOfIndex = counter;
+	return index;
 }
 
 double F(double r)
@@ -110,10 +207,6 @@ double E(double *X, double *Y, double *Z, size_t size, int e)
 			r = sqrt(pow((X[e] - X[i]), 2) + pow((Y[e] - Y[i]), 2) + pow((Z[e] - Z[i]), 2));
 			E += F(r);
 		}
-		else
-		{
-			E = E;
-		}
 	}
 	return E;
 }
@@ -121,10 +214,7 @@ double E(double *X, double *Y, double *Z, size_t size, int e)
 double Energy(double *X, double *Y, double *Z, size_t size)
 {
 	double U = 0;
-	for (unsigned int i = 0; i < size; i++)
-	{
-		U += E(X, Y, Z, size, i);
-	}
+	for (unsigned int i = 0; i < size; i++) U += E(X, Y, Z, size, i);
 	U /= 2;
 	return U;
 }
